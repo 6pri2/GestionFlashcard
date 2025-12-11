f9fe141cf9590d3db10ba7b3575b784330e2a7a2
Cyprien <cyprien.duroy@gmail.com>
2025-12-11 09:03:51 +0100

    create database and users table

---
+++ src/db/seed.js
@@ -0,0 +1,42 @@
+import { db } from './db.js'
+import { users } from './schema.js'
+import bcrypt from 'bcrypt'
+
+async function seed() {
+	console.log('Starting database seed...')
+
+	try {
+		await db.delete(users)
+
+		const hashedPassword1 = await bcrypt.hash('motdepasse', 12)
+		const hashedPassword2 = await bcrypt.hash('12345678', 12)
+
+		const seedUsers = [
+			{
+				email : 'test@test.com',
+				firstname : 'alexandre',
+				lastname : 'LeRoy',
+				password : hashedPassword1,
+			},
+			{
+				email : 'test2@test.com',
+				firstname : 'Cyprien',
+				lastname : 'Duroy',
+				password : hashedPassword2,
+				admin : true
+			}
+		]
+
+
+		// db.insert(users).values(seedUsers).returning().then((result) => console.log(result))
+		const result = await db.insert(users).values(seedUsers).returning()
+
+		console.log('Database seeded successfully!')
+		console.log('email : ' . result)
+		console.log('password : motdepasse')
+	} catch (error) {
+		console.error('Error seeding database:', error)
+	}
+}
+
+seed()
