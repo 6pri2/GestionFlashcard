4bdb0dc18316a624c7316833e32c68838fa6aeb5
Alexandre Le Roy <alexandre.leroy01@etu.unicaen.fr>
2025-12-11 09:10:06 +0100

    setup files for authentification

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import usersRouter from './routers/authRouter'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000;
+
+app.use('/auth', authRouter)
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`)
