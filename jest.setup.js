c13e541c091bfbab4c44da7fb47587a1658f323d
Cyprien <cyprien.duroy@gmail.com>
2026-01-05 17:00:30 +0100

    make seeding after test

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,10 @@
 import { seed } from './src/db/seed.js';
 
 beforeAll(async () => {
-  console.log('Seeding test database...');
+  await seed();
+});
+
+
+afterAll(async () => {
   await seed();
 });
