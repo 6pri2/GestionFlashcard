5c74a6d179c9954241565e72ac80223afbe972f4
Le Roy Alexandre <alexandre05101410@gmail.com>
2026-01-07 22:47:40 +0100

    fix isAdmin middleware

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -5,7 +5,8 @@
  * @param {import("express").NextFunction} next
  */
 export const isAdmin = (req, res, next) => {
-    if (req.user && req.user.admin === true) {
+    console.log(req.user.admin)
+    if (req.user && req.user.userAdmin === true) {
         next();
     } else {
         res.status(403).json({ message: "Accès refusé. Vous devez être administrateur pour effectuer cette action." });
